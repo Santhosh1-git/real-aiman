1276a0b9235746b14811b163481a7fcaa3600e2b
Santhosh1-git <masterppn123@gmail.com>
2025-01-26 17:54:00 +0530

    change nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,38 +11,39 @@ function Header() {
     <Container fluid >  
         <Row id={'header_bg'} >
           <Col >
-          <ul className='header_info' >
-            <li>
-          <a><i className="fa-solid fa-phone">  </i>   96 2974 6444</a>
-            </li>
-            <li>
-            <a><i className="fa-solid fa-envelope"> </i>  principal@aimancollege.edu.in</a>          
-            </li>
-            <li>
-            <a><i className="fa-brands fa-facebook px-2 "></i> <i className="fa-brands fa-instagram px-2  "></i> <i className="fa-brands fa-youtube px-1  "></i></a>
-            </li>
-            <li>
-            <a> <i class="fa-regular fa-file"></i> NAAC - Appeal</a>
-            </li>
-            <li>
-            <a> <i class="fa-regular fa-file"></i>  IQAC</a>
-            </li>
-            <li>
-            <a> <i class="fa-regular fa-file"></i>  NACC</a>
-            </li>
-            <li>
-            <a> <i className="fa-solid fa-book"></i> ICT </a>
-            </li>
-            <li>
-            <a> <i className="fa-solid fa-user"></i>  IIC</a>
-            </li>
-            <li>
-            <a> <i className="fa-solid fa-lock"></i>  RTI</a>
-            </li>
-            <li className='login'>
-            <span  >  <i className="fa-regular fa-user"></i> ERP login</span>
-            </li>
-          </ul>
+          <nav class="navbar navbar-expand-lg">
+    <div class="collapse navbar-collapse" >
+      <ul class="navbar-nav  me-auto  ">
+        <li class="nav-item">
+        <a><i className="fa-solid fa-phone">  </i>   96 2974 6444</a>
+        </li>
+        <li class="nav-item">
+        <a><i className="fa-solid fa-envelope"> </i>  principal@aimancollege.edu.in</a>          
+        </li>
+        <li class="nav-item ">
+        <a><i className="fa-brands fa-facebook px-2 "></i> <i className="fa-brands fa-instagram px-2  "></i> <i className="fa-brands fa-youtube px-1  "></i></a>
+        </li>
+        <li class="nav-item">
+        <a> <i class="fa-regular fa-file"></i> NAAC - Appeal</a>
+        </li>
+        <li class="nav-item">
+        <a> <i class="fa-regular fa-file"></i>  IQAC</a>
+        </li>
+        <li class="nav-item">
+        <a> <i class="fa-regular fa-file"></i>  NACC</a>
+        </li>
+        <li class="nav-item">
+        <a> <i className="fa-solid fa-book"></i> ICT </a>
+        </li>
+        <li class="nav-item">
+        <a> <i className="fa-solid fa-lock"></i>  RTI</a>
+        </li>
+        <li class="nav-item login">
+        <span  >  <i className="fa-regular fa-user"></i> ERP login</span>
+        </li>
+      </ul>
+    </div>
+</nav>
           </Col>
         </Row>
       </Container>   
@@ -66,50 +67,9 @@ function Header() {
       </Container>
       <hr />
       {/* main header */}
-      {/* <div  >
-        <Container fluid >
-        <Row>
-          <Col>
-            <ul className="link_header" >
-              <li><a className='main_link' > home </a></li>
-              <li><a className='main_link' > about us </a></li>
-              <li><a className='main_link' > addmissions</a></li>
-              <li><a className='main_link' > department </a></li>
-              <li><a className='main_link' >library </a></li>
-              <li><a className='main_link' >academics</a></li>
-              <li><a className='main_link' > campus life</a></li>
-            </ul>
-          </Col>
-        </Row>
-        </Container>
-      </div> */}
+     
 
-      <nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    {/* <div class="collapse navbar-collapse" id="navbarSupportedContent"> */}
-      <ul class="navbar-nav  me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">about us</a>
-        </li>
-        <li class="nav-item ">
-        <a class="nav-link" href="#">addmissions</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">department</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">library</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">campus life</a>
-        </li>
-      </ul>
-    </div>
-  {/* </div> */}
-</nav>
+      
     </>
   )
 }
